3a11a58f61f2640274640bcb4809022b86719b69
designravenous <32999911+designravenous@users.noreply.github.com>
2018-08-01 21:06:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 
 const fs = require('fs');
 const Textclass = require('./module');
-var m2 = Textclass;
+var mclass = Textclass;
 
-var k = new m2.Filehandling();
-
+var k = new mclass.Filehandling();
 
 var textDocument = "text.txt";
 
 var inputText = "\r\nText line input for textDocument!";
-var time = m2.slogan;
-
-
+var time = mclass.slogan;
 
 k.Add_Text_Method(textDocument,inputText);
 k.Read_File_Method(textDocument);
