2119b6d230397a8fc65deaca3515107871ecd8a0
designravenous <32999911+designravenous@users.noreply.github.com>
2018-08-03 12:24:41 +0200

    Update module.js

--- module.js
+++ module.js
@@ -6,7 +6,7 @@ class DNS_CLASS{
     NSlookUp(destination, filename){
         dns.lookup(destination, (err,address, family)=>{
             if(err) return err;
-            var results = "\r\naddress for: " + destination + " = " + address + " IPv" + family;
+            var results = "\r\nnsLookUp for: " + destination + " = " + address + " IPv" + family;
             console.log("address for: " + destination + " = " + address);
             fs.appendFile(filename, results, function(err){
                 if(err) return err;
@@ -27,19 +27,39 @@ class DNS_CLASS{
             })
         })
     }
+    TimeStamp(filename){
+        let time ={};
+        time.year = new Date().getFullYear();
+        time.month = new Date().getMonth();
+        time.day = new Date().getDate();
+        time.hour = new Date().getHours();
+        time.minute = new Date().getMinutes();
+        time.seconds = new Date().getSeconds();
+        var H_our;
+        var M_minute;
+        var S_econd;
+        if(time.hour < 10){
+            H_our = "0" + time.hour;
+        }else{
+            H_our = time.hour;
+        }
+        if(time.minute < 10){
+            M_minute = "0" + time.minute;
+        }else{
+            M_minute = time.minute;
+        }
+        if(time.seconds < 10){
+            S_econd = "0" + time.seconds;
+        }else{
+            S_econd = time.seconds;
+        }
+        var current_time = "\r\n" + time.day + "/" + time.month + "/" + time.year + " " + H_our + ":" + M_minute + ":" + S_econd;
+        fs.appendFile(filename, current_time, function(err){
+            if(err) return err;
+            console.log(current_time);
+        } )
+    }
 }
-
-let time ={};
-time.year = new Date().getFullYear();
-time.month = new Date().getMonth();
-time.day = new Date().getDate();
-time.hour = new Date().getHours();
-time.minute = new Date().getMinutes();
-time.seconds = new Date().getSeconds();
-
-var slogan = time.day + "/" + time.month + "/" + time.year + " Time: " + time.hour + ":" + time.minute + ":" + time.seconds;
-
 module.exports.DNS_CLASS = DNS_CLASS;
-module.exports.slogan = slogan;
 
 
