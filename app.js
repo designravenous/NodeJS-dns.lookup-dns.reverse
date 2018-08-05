9bf19d60be05cf5f21ff2e3cf077cc526be1410d
designravenous <32999911+designravenous@users.noreply.github.com>
2018-08-05 16:21:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const fs = require("fs");
 const modules = require("./module");
 
 const filename = "dns_information.xls";
+var filepath = "./dns_information.xls";
 
 let ip = {};
-ip.One;
+ip.One = "193.235.151.130";
 ip.Two = "193.234.151.142";
 
 let destination = {};
 destination.One = "aftonbladet.se";
+destination.Two = "faktiskt.se";
 
 var Search = new modules.DNS_CLASS();
+var File = new modules.FileSystem();
 
 Object.keys(destination).forEach(function(key) {
     Search.NSlookUp(destination[key], filename);
@@ -20,6 +23,7 @@ Object.keys(destination).forEach(function(key) {
   Object.keys(ip).forEach(function(key) {
     Search.ReverseLookUp(ip[key], filename);
   });
-  
-  Search.TimeStamp(filename);
+
+File.ReadFile(filepath);
+Search.TimeStamp(filename);
 
